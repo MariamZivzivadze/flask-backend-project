fd3cdf634f9b7a210addbc010b0e6bbefcacfb03
Mariam Zivzivadze <zivzivadzemariam@yahoo.com>
2026-09-06 17:22:11 +0400

    feat: add Post model and create_post route

--- models.py
+++ models.py
@@ -5,3 +5,10 @@ class User(db.Model):
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(200), nullable=False)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    author = db.relationship('User', backref='posts')
